b85cf6d9400ea06beba4aeb552279c1a844a7b4c
Luciana Silva <luciana.silva@qwasolucoes.com.br>
2022-05-26 14:46:05 -0300

    Testando exclusão de uma pessoa registrada anteriormente

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -14,8 +14,10 @@ public class Program {
 		EntityManager em = emf.createEntityManager();
 
 		Pessoa p = em.find(Pessoa.class, 2);
-		
-		System.out.println(p);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		System.out.println("Pronto");
 		em.close();
 		emf.close();
 	}
